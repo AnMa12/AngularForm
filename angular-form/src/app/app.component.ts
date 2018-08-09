8e764616fe539d0383574a6a64fcb2a178c3cb7a
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-08-09 09:53:03 +0300

    TODO: diagnostic interpolation
    diagnostic interpolation after the input tag so you can see what you're doing

--- angular-form/src/app/app.component.ts
+++ angular-form/src/app/app.component.ts
@@ -6,14 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  clickMessage = '';
-  values = '';
 
-  onKey(event: any) { // without type info
-    this.values += event.target.value + ' | ';
-  }
-
-  onClickMe() {
-    this.clickMessage = 'You are my hero!';
-  }
 }
