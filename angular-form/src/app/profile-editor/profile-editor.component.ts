0bcb5a792fa6d328e38f9a5e83897c5ad8f3e9c9
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-08-09 12:02:23 +0300

    Reactive forms -  Form builder
    Reactive forms are synchronous, and template-driven forms are asynchronous.

--- angular-form/src/app/profile-editor/profile-editor.component.ts
+++ angular-form/src/app/profile-editor/profile-editor.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
@@ -7,28 +10,27 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./profile-editor.component.css']
 })
 export class ProfileEditorComponent {
-  profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    address: new FormGroup({
-      street: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zip: new FormControl('')
-    })
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
   });
 
-  onSubmit() {
-    // TODO: Use EventEmitter with form value
-    console.warn(this.profileForm.value);
+  constructor(private fb: FormBuilder) { }
+
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
   }
 
-  updateProfile() {
-    this.profileForm.patchValue({
-      firstName: 'Nancy',
-      address: {
-        street: '123 Drew Street'
-      }
-    });
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
   }
 }
