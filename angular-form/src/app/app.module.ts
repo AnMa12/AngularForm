415678a0459540a0c5d5c2381c21f79be1cdde28
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-11-28 00:01:42 +0200

    updates

--- angular-form/src/app/app.module.ts
+++ angular-form/src/app/app.module.ts
@@ -7,19 +7,28 @@ import { HeroFormComponent } from './hero-form/hero-form.component';
 import { HeroFormTemplateComponent } from './hero-form-template/hero-form-template.component';
 import { NameEditorComponent } from './name-editor/name-editor.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { AutocompleteComponent } from './autocomplete/autocomplete.component';
 
+import { MatFormFieldModule, MatInputModule, MatAutocompleteModule } from '@angular/material';
+import { BrowserAnimationsModule} '@angular/platform-browser/animations';
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    BrowserAnimationsModule
+
   ],
   declarations: [
     AppComponent,
     HeroFormComponent,
     HeroFormTemplateComponent,
     NameEditorComponent,
-    ProfileEditorComponent
+    ProfileEditorComponent,
+    AutocompleteComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-form/src/app/profile-editor/profile-editor.component.ts
+++ angular-form/src/app/profile-editor/profile-editor.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
@@ -8,7 +10,7 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class ProfileEditorComponent {
 
-  profileForm = new FormGroup({
+  /*profileForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
     address: new FormGroup({
@@ -17,7 +19,20 @@ export class ProfileEditorComponent {
       state: new FormControl(''),
       zip: new FormControl('')
     })
+  });*/
+
+  profileForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    address: this.formBuilder.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
   });
+
+  constructor(private formBuilder: FormBuilder) { }
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
